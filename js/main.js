82ce1fa575ce2ce925a35b8ff594f8d246695937
Rodrigo Soares <adsrodsoares@gmail.com>
2021-01-16 15:54:15 -0300

    Renderizing variable in DOM

--- js/main.js
+++ js/main.js
@@ -9,11 +9,7 @@ var weight = tdWeight.textContent;
 var tdHeight = patient.querySelector(".info-altura")
 var height = tdHeight.textContent;
 
+var tdImc = patient.querySelector(".info-imc")
 var imc = weight/(Math.pow(height, 2))
+tdImc.textContent = imc;
 
-console.log(imc)
-
-/*Revisando: o document.querySelector vai numa tag HTML e extrai o conteúdo dela pro JS (na integra, com todas as
-as tags). Depois, eu salvo esse conteúdo numa variável. Depois eu chamo a variavel.querySelector para escolher o
-id ou a class desejado e guardo numa nova variável. Por fim, eu invoco o variavel.textContent para extrair somente
-o texto de dentro da tag.*/
