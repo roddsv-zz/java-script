233b72a46a250af5b5e8c056677a93de792c9f7f
Rodrigo Soares <adsrodsoares@gmail.com>
2021-01-16 15:42:52 -0300

    Using querySelector and textContent

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 var title = document.querySelector(".title")
-
 title.textContent = "Aparecida Nutricionista"
+
+var patient = document.querySelector("#first-patient")
+
+var tdWeight = patient.querySelector(".info-peso");
+var weight = tdWeight.textContent;
+
+var tdHeight = patient.querySelector(".info-altura")
+var height = tdHeight.textContent;
+
+var imc = weight/(Math.pow(height, 2))
+
+console.log(imc)
+
+/*Revisando: o document.querySelector vai numa tag HTML e extrai o conteúdo dela pro JS (na integra, com todas as
+as tags). Depois, eu salvo esse conteúdo numa variável. Depois eu chamo a variavel.querySelector para escolher o
+id ou a class desejado e guardo numa nova variável. Por fim, eu invoco o variavel.textContent para extrair somente
+o texto de dentro da tag.*/
